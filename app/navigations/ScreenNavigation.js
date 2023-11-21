de908e9cbcb77d0541471b276a59901c6fff9e8d
Bappi Kabir <kabirbapson@gmail.com>
2023-11-21 17:02:54 +0100

    Go News done

--- app/navigations/ScreenNavigation.js
+++ app/navigations/ScreenNavigation.js
@@ -5,6 +5,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
+
 // screens
 import Home from '../screens/Home';
 import Location from '../screens/Location';
