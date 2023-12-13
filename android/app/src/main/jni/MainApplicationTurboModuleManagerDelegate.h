6f6acb759dea6d747c3c3630a16b40403311317c
Bappi Kabir <kabirbapson@gmail.com>
2023-12-13 04:14:21 +0100

    renamed project

--- android/app/src/main/jni/MainApplicationTurboModuleManagerDelegate.h
+++ android/app/src/main/jni/MainApplicationTurboModuleManagerDelegate.h
@@ -14,7 +14,7 @@ class MainApplicationTurboModuleManagerDelegate
  public:
   // Adapt it to the package you used for your Java class.
   static constexpr auto kJavaDescriptor =
-      "Lcom/goproject/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate;";
+      "Lcom/goproject/news/news/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate;";
 
   static jni::local_ref<jhybriddata> initHybrid(jni::alias_ref<jhybridobject>);
 
--- android/app/src/main/jni/MainComponentsRegistry.h
+++ android/app/src/main/jni/MainComponentsRegistry.h
@@ -13,7 +13,7 @@ class MainComponentsRegistry
  public:
   // Adapt it to the package you used for your Java class.
   constexpr static auto kJavaDescriptor =
-      "Lcom/goproject/newarchitecture/components/MainComponentsRegistry;";
+      "Lcom/goproject/news/news/newarchitecture/components/MainComponentsRegistry;";
 
   static void registerNatives();
 
