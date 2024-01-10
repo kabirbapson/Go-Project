429f31c8552d0268e98ec24a84dee72c129a1f51
Bappi Kabir <kabirbapson@gmail.com>
2024-01-10 06:37:37 +0100

    build fixed

--- app/screens/Splash/index.js
+++ app/screens/Splash/index.js
@@ -1,8 +1,7 @@
 import React, {useEffect} from 'react';
-import {Avatar, Box, Image, Text, VStack} from 'native-base';
+import {Image, Text, VStack} from 'native-base';
 import gMap from '../../../assets/images/GomeMap.png';
 import gLogo from '../../../assets/images/GombeLogo.png';
-import hhKumo from '../../../assets/images/yo.jpg';
 
 const Splash = ({navigation}) => {
   useEffect(() => {
@@ -12,17 +11,8 @@ const Splash = ({navigation}) => {
   }, []);
 
   return (
-    <VStack
-      bg={'red.100'}
-      justifyContent={'space-around'}
-      alignItems={'center'}
-      flex={1}>
+    <VStack justifyContent={'space-around'} alignItems={'center'} flex={1}>
       <Image source={gLogo} mt={'3%'} alt="gLogo" h={140} w={140} />
-
-      {/* <Text mt={'2%'} color={'#2A9D8F'} fontSize={20} fontWeight={'black'}>
-        GoProject
-      </Text> */}
-
       <Image
         source={gMap}
         alt="gmap"
@@ -30,21 +20,7 @@ const Splash = ({navigation}) => {
         w={'100%'}
         h={'55%'}
       />
-
-      {/* <Avatar mt={'7%'} size={'xl'} source={hhKumo} alt="gmap" /> */}
-      <Box
-        bg={'teal.600'}
-        rounded={10}
-        w={'full'}
-        alignItems={'center'}
-        // position={'absolute'}
-        // bottom={10}
-      >
-        <Text>Inspired by Bappi</Text>
-        {/* <Text color={'#2A9D8F'} fontSize={20} fontWeight={'black'}>
-          Hon. Habib Hassan Kumo
-        </Text> */}
-      </Box>
+      <Text>Inspired by Bappi</Text>
     </VStack>
   );
 };
